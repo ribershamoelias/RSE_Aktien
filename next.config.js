a832164682cb504f7b5e5f523b86e10845c619a3
Riber Shamo Elias <163605646+ribershamoelias@users.noreply.github.com>
2025-06-19 20:29:01 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// Next.js + i18n Einstellungen/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  i18n: {
-    locales: ['de', 'en'],
-    defaultLocale: 'de',
-  },
-};
