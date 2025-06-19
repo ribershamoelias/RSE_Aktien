dc063ad3ac9e6b99a96dc63d967a277dce3bf650
Riber Shamo Elias <163605646+ribershamoelias@users.noreply.github.com>
2025-06-19 20:12:38 +0200

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+// pages/_app.tsx
+import '@/styles/globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
